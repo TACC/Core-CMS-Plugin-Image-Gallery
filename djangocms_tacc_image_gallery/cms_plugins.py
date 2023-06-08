eea08403e11f02e66f0b320a4b7a72d5431a67d9
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-06-08 16:17:29 -0500

    fix: remove copy-pasta `translations`

--- djangocms_tacc_image_gallery/cms_plugins.py
+++ djangocms_tacc_image_gallery/cms_plugins.py
@@ -1,8 +1,6 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
-from django.utils.translation import gettext as _
-
 @plugin_pool.register_plugin
 class TaccsiteImageGalleryPlugin(CMSPluginBase):
     """
