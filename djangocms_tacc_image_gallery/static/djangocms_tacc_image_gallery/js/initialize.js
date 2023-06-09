a40a238145b712618054ff9a81fdac472a679699
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-06-08 21:39:56 -0500

    chore: remove javascript image link wrapping

--- djangocms_tacc_image_gallery/static/djangocms_tacc_image_gallery/js/initialize.js
+++ djangocms_tacc_image_gallery/static/djangocms_tacc_image_gallery/js/initialize.js
@@ -1,40 +1,15 @@
 import lightGallery from 'https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.1/lightgallery.es5.min.js';
 
 /**
-* Elements that contain images for gallery
-* @type {NodeList}
-*/
+ * Elements that contain images for gallery
+ * @type {NodeList}
+ */
 const galleryContainers = document.getElementsByClassName('lightgallery');
 
 /**
-* To wrap images in links to full size image
-* @param [HTMLElement] gallery - The gallery container
-*/
-function wrapImagesInLinks ( gallery ) {
-    const unlinkedImages = gallery.querySelectorAll('img:not(a > img)');
-    const fragment = document.createDocumentFragment();
-
-    unlinkedImages.forEach( image => {
-        const anchor = document.createElement('a');
-        const href = image.currentSrc;
-
-        console.log({ image, anchor, href });
-
-        /* To build anchor */
-        anchor.href = href;
-        anchor.target = '_blank';
-        fragment.appendChild( anchor );
-
-        /* To wrap image */
-        image.parentNode.replaceChild( anchor, image );
-        anchor.appendChild( image );
-    });
-}
-
-/**
-* To initialize a single gallery
-* @param [HTMLElement] gallery - The gallery container
-*/
+ * To initialize a single gallery
+ * @param [HTMLElement] gallery - The gallery container
+ */
 function initGallery ( gallery ) {
     lightGallery( gallery, {
         // plugins: [ lgZoom, lgThumbnail ],
@@ -44,7 +19,4 @@ function initGallery ( gallery ) {
     });
 }
 
-[ ...galleryContainers ].forEach( container => {
-    wrapImagesInLinks( container );
-    initGallery( container );
-});
+[ ...galleryContainers ].forEach( initGallery );
